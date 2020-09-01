5519fd952b1f1c52c3cc60c03f6a6b35f40370ac
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-09-01 15:12:42 +0900

    Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ allprojects {
     kotlinGradle {
       ktlint("0.38.0").userData(mapOf("indent_size" to "2", "continuation_indent_size" to "2"))
 
-      licenseHeaderFile("${rootProject.projectDir}/config/license/header.java", "plugins|include")
+      licenseHeaderFile("${rootProject.projectDir}/config/license/header.java", "plugins|include|import")
     }
   }
 
